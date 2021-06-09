0bea0a280cda623da96a6eba3d9009b9a738ab4a
slybootslion <slybootslion@vip.qq.com>
2021-06-09 16:28:22 +0800

    2021-06-09 16:28:22

--- app/api/travel-react/auth.js
+++ app/api/travel-react/auth.js
@@ -1,23 +1,24 @@
 import KoaRouter from 'koa-router'
 import { NoParamsValidator } from '../../validator'
-import { getParamsByV } from "../../utils";
-import TravelAuthDao from "../../dao/travel-react/AuthDao";
+import { getParamsByV } from '../../utils'
+import TravelAuthDao from '../../dao/travel-react/AuthDao'
+import { luCodeRequired } from '../../middleware/check-code'
 
 const travelAuthApi = new KoaRouter({
   prefix: '/travel/auth',
 })
 
-travelAuthApi.post('/login', async ctx => {
+travelAuthApi.post('/login', luCodeRequired, async ctx => {
   const v = await new NoParamsValidator().validate(ctx)
   const data = getParamsByV(v)
   ctx.json(data)
 })
 
-travelAuthApi.post('/register', async ctx => {
+travelAuthApi.post('/register', luCodeRequired, async ctx => {
   const v = await new NoParamsValidator().validate(ctx)
   const { email, password, confirmPassword } = getParamsByV(v)
   const tad = new TravelAuthDao()
-  const data = await tad.register({email, password, confirmPassword})
+  const data = await tad.register({ email, password, confirmPassword })
   ctx.json({ email, password, confirmPassword })
 })
 
