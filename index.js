566fd70a600d8c494e545e8e3858b49ac43a92ca
slybootslion <slybootslion@vip.qq.com>
2023-03-01 17:06:03 +0800

    啥也不是

--- index.js
+++ index.js
@@ -1,27 +1,11 @@
 import * as Cheerio from 'cheerio'
 import { createFile, delayTimer, http } from "./tools.js";
 
-const urls = [
-  '16664.html',
-  '16486.html',
-  '15124.html',
-  '11678.html',
-  '16435.html',
-  '16656.html',
-  '16901.html',
-  '15779.html',
-  '16839.html',
-  '17706.html',
-  '17101.html',
-  '8368.html',
-  '12189.html',
-  '15906.html',
-  '8074.html',
-]
-
-// 点赞排序 王后的巴掌
-
 const runQuJi = async () => {
+  const urls = [
+    '20199.html',
+  ]
+
   const baseUrl = 'https://ifun.cool/'
 
   for (const url of urls) {
@@ -39,4 +23,40 @@ const runQuJi = async () => {
   }
 }
 
-runQuJi()
+const runYanShen = async () => {
+  const urls = [
+    '2023/03/01/如何以「我将来是要做皇后的」为开头，写一篇故事',
+    '2023/03/01/等烟雨',
+    '2023/03/01/有哪些女主把男主当替身的小说？',
+    '2023/03/01/你知道哪些齁甜的古言小说',
+    '2023/03/01/有什么甜到嘴角疯狂上扬的小说-',
+    '2023/03/01/可以推荐一些高甜文笔好的小说吗-江枫',
+    '2023/02/28/那些被辜负的深情后来都怎么样了',
+    '2023/02/28/求男主占有欲强偏执心狠手辣的小说！！！',
+    '2023/02/28/有没有听完脊背发凉的鬼故事',
+    '2023/02/28/什么样的女人才是聪明女人小尘',
+  ]
+
+  const baseUrl = 'https://onehu.xyz/'
+
+  for (const url of urls) {
+    await delayTimer(1200)
+    const res = await http(baseUrl + url)
+    const $ = Cheerio.load(res);
+    const arr = url.split('/')
+    const title = arr[arr.length - 1]
+    const contentEls = $('.markdown-body p')
+    let content = ''
+    for (let i = 0; i < contentEls.length; i++) {
+      const txt = $(contentEls[i]).text()
+      !!txt.trim() && (content += txt + '\n')
+    }
+    createFile({ title, content })
+  }
+}
+
+// 集趣
+// runQuJi()
+
+// 盐神
+runYanShen()
