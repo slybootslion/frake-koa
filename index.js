de510c471ed4038700c76b0c4e53e18c53185257
slybootslion <slybootslion@vip.qq.com>
2023-03-25 16:58:50 +0800

    可以直接爬取知乎盐选的文章了，不过cookie还是要自己弄，文章链接要自己的放，凑合用吧，不过已经很好了。

--- index.js
+++ index.js
@@ -55,15 +55,32 @@ const runYanShen = async () => {
 // 盐选
 const runYanxuan = async () => {
   const urls = [
-    '1465372641737154560/section/1481962063962062848',
+    'https://www.zhihu.com/market/paid_column/1465372641737154560/section/1479777849841905664',
+    'https://www.zhihu.com/market/paid_column/1465372641737154560/section/1476810743610187776',
+    'https://www.zhihu.com/market/paid_column/1537870091269545984/section/1539970276040736768',
+    'https://www.zhihu.com/market/paid_column/1583895664110620672/section/1597243127542321152',
+    'https://www.zhihu.com/market/paid_column/1611773869718745089/section/1617130738939314176',
+    'https://www.zhihu.com/market/paid_column/1611773869718745089/section/1622259427502882816',
+    'https://www.zhihu.com/market/paid_column/1573680063303835648/section/1576621399095107584',
+    'https://www.zhihu.com/market/paid_column/1614278915110703104/section/1614295814507130881',
+    'https://www.zhihu.com/market/paid_column/1614278915110703104/section/1614281718311219200',
+    'https://www.zhihu.com/market/paid_column/1614278915110703104/section/1614305185030688768',
+    'https://www.zhihu.com/market/paid_column/1614278915110703104/section/1616089697268252672',
+    'https://www.zhihu.com/market/paid_column/1573680063303835648/section/1573683464150417409',
+    'https://www.zhihu.com/market/paid_column/1573680063303835648/section/1576263141973024768',
+    'https://www.zhihu.com/market/paid_column/1573680063303835648/section/1573682789873827840',
+    'https://www.zhihu.com/market/paid_column/1573680063303835648/section/1576627786713980928',
+    'https://www.zhihu.com/market/paid_column/1492523308218732544/section/1580972282805260288',
+    'https://www.zhihu.com/market/paid_column/1486805707345735680/section/1505567867630669824',
+    'https://www.zhihu.com/market/paid_column/1486805707345735680/section/1527745634027581440',
   ]
 
-  const baseUrl = 'https://www.zhihu.com/market/paid_column/'
+  const baseUrl = ''
   for (const url of urls) {
-    await delayTimer(1200)
+    await delayTimer(1900)
     const res = await httpCookie(baseUrl + url)
     const $ = Cheerio.load(res.res.text);
-    const title = $('.ManuscriptTitle-root-gcmVk').text()
+    const title = $('.ManuscriptTitle-root-gcmVk').text() + ' - ' + $('.ProductCardNew-title-r3FmE').text()
     const contentEls = $('#manuscript p')
     let content = ''
     for (let i = 0; i < contentEls.length; i++) {
